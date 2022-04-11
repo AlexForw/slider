4968ae3cb80067a93cf4e6d366b28c0408f7c741
AlexForw <lmityushkin@mail.ru>
2022-04-11 15:16:18 +0300

    feat: add react

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,25 @@ import { useRef, useState, useEffect } from 'react'
 import images from './images'
 
 function App() {
-  console.log(images)
+  const [width, setWidth] = useState(0)
+  const block = useRef()
+  useEffect (() => {
+    setWidth(block.current.scrollWidth - block.current.offsetWidth)
+  },[])
   return (
-    <div className="Container">
-      <motion.div className='block'>
-        {images.map((img, i) => {
-          return(<motion.img className='image' key={i + img} src={img} animate={{x:75}} alt="nature" />)
-        })}
+    <div className='App'>
+      <motion.div className="wrapper"><motion.div ref={block} className="container">
+        <motion.div drag='x' dragConstraints={{right:0,left: -width}} className='block'>
+          {images.map((img, i) => {
+            return (
+            <motion.div className="item"><img className='image' key={i + img} src={img} animate={{ x: 20 }} alt="nature" /></motion.div>
+            )
+          })}
+        </motion.div>
+      </motion.div>
       </motion.div>
     </div>
-  );
+      );
 }
 
-export default App;
+      export default App;
